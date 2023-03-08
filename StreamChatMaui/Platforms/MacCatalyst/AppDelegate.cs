53f0c77fec9736c7785ded94eb02457476a92b2d
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2023-03-08 22:34:02 +0100

    Create default .NET MAUI project

---
+++ StreamChatMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace StreamChatMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
