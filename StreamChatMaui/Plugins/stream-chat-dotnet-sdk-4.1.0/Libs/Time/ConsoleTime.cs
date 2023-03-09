31f65dcf51b922e70aa9afd6d8180ca185495da4
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2023-03-09 14:57:51 +0100

    Remove `System.Diagnostics` dependency - was causing errors on Android build

--- StreamChatMaui/Plugins/stream-chat-dotnet-sdk-4.1.0/Libs/Time/ConsoleTime.cs
+++ StreamChatMaui/Plugins/stream-chat-dotnet-sdk-4.1.0/Libs/Time/ConsoleTime.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Diagnostics;
-
-namespace StreamChat.Libs.Time
+﻿namespace StreamChat.Libs.Time
 {
     /// <summary>
     /// <see cref="ITimeService"/> based on <see cref="UnityEngine.Time"/>
     /// </summary>
     public class ConsoleTime : ITimeService
     {
-        public float Time => (float)(DateTime.UtcNow - Process.GetCurrentProcess().StartTime.ToUniversalTime()).TotalSeconds;
+        public float Time => (float)(DateTime.UtcNow - _timeStarted).TotalSeconds;
         public float DeltaTime => 0.1f;
+
+        private readonly DateTime _timeStarted = DateTime.UtcNow;
     }
 }
